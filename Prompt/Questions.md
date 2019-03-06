8c1ceab1c89c8052b93f3999721df0ff18238528
bernat <elbernat@gmail.com>
2019-03-06 10:53:01 -0500

    Initial pack of questions in

--- Prompt/Questions.md
+++ Prompt/Questions.md
@@ -7,8 +7,16 @@
 * Do you feel rested?
 * Do you feel focused enough to trade today?
 1. LOCATION - FIBS
+* Are we in a range or trending state?
+* Using Retracement FIB did you locate your RLZ?
+* Using Extension FIB did you locate your RLZ?
 2. DIVERGENCE - Contrarian Momentum
+* Do you find divergence in the RSI indicator?
+* Do you find divergence in the OVB indicator?
 3. FRACTAL - M,W,H&S
+* Can you discern any fractal / Inside or Outside canfdle pattern?
+* Can you see clearly if we are going to break an 'M'?
+* Can you see clearly if we are going to break an 'W'?
 4. CANDLES - inner, outer
 5. ELLIOT WAVES
 6. RISK REWARD RATIO - Minimum 2.0?
--- Prompt/index.js
+++ Prompt/index.js
@@ -25,7 +25,61 @@ let questions = [
         message: 'Do you feel focused enough to trade today?',
         validate: resp => resp == 'no' ? 'Today you can not trade buddy' : true,
         initial: 'yes or no'
-    }
+    },
+    {
+        type: 'text',
+        name: 'market state',
+        message: 'Are we in a range or trend state?',
+        initial: 'tell me about the market movement'
+    },
+    {
+        type: 'text',
+        name: prev => prev == 'range' ? 'range RLZ' : 'trend RLZ',
+        message: (prev) => prev == 'range' ? 'did you apply fib retracement and find RLZ?' : 'did you apply fib extension and find RLZ?',
+        initial: 'yes or no'
+    },
+    {
+        type: 'text',
+        name: 'RSI divergence',
+        message: 'Do you find divergence in the RSI indicator?',
+        // validate: resp => resp == 'no' ? 'Today you can not trade buddy' : true,
+        initial: 'yes or no'
+    },
+    {
+        type: 'text',
+        name: 'OBV diveregence',
+        message: 'Do you find divergence in the OVB indicator?',
+        // validate: resp => resp == 'no' ? 'Today you can not trade buddy' : true,
+        initial: 'yes or no'
+    },
+    {
+        type: 'text',
+        name: 'Fractals',
+        message: 'Can you discern any fractal / Inside or Outside canfdle pattern?',
+        // validate: resp => resp == 'no' ? 'Today you can not trade buddy' : true,
+        initial: 'yes or no'
+    },
+    {
+        type: 'text',
+        name: 'M',
+        message: 'Can you see clearly if we are going to break an "M"?',
+        // validate: resp => resp == 'no' ? 'Today you can not trade buddy' : true,
+        initial: 'yes or no'
+    },
+    {
+        type: 'text',
+        name: 'W',
+        message: 'Can you see clearly if we are going to break an "W"?',
+        // validate: resp => resp == 'no' ? 'Today you can not trade buddy' : true,
+        initial: 'yes or no'
+    },
+    {
+        type: 'text',
+        name: 'Risk Reward Ratio 2:1',
+        message: 'Can you get a minimum of 2:1 Risk Reward Ratio?',
+        validate: resp => resp == 'no' ? 'You cannot take this trade buddy' : true,
+        initial: 'yes or no'
+    },
 ];
 
 (async () => { 
@@ -34,13 +88,13 @@ let questions = [
     // NODE: Write answer in JSON 
     let answer_json = JSON.stringify(response)
     fs.writeFile('./sample.json', answer_json, () => {
-        console.log('response: ', response)
+        console.log('saved response: ', response)
         })
     // NODE: Read answer from JSON
     fs.readFile('./sample.json', (err, data) => {  
         if (err) throw err;
         p_content = JSON.parse(data)
-        console.log(p_content);
+        console.log('loaded response: ',p_content);
         })
     }  
 )()
